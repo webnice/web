4151c270eb2c19ece06876867f875d34bdc755c2
Alex Geer <monoflash@gmail.com>
2017-06-18 04:46:34 +0300

    Added tests

--- webext17.go
+++ webext17.go
@@ -9,13 +9,17 @@ import (
 )
 
 // Stop web server
-func (wsv *web) Stop() {
+func (wsv *web) Stop() Interface {
 	if wsv.listener != nil {
 		wsv.err = wsv.listener.Close()
 	}
+	return wsv
 }
 
 func (wsv *web) loadConfiguration() *http.Server {
+	if wsv.route.Errors().RouteConfigurationError(nil) != nil {
+		wsv.err = wsv.route.Errors().RouteConfigurationError(nil)
+	}
 	return &http.Server{
 		Addr:           wsv.conf.HostPort,
 		ReadTimeout:    wsv.conf.ReadTimeout,
