cf5b8bbd7fe1dfa8926890e96452c6c53d9799ac
Alex Geer <monoflash@gmail.com>
2019-02-15 19:34:48 +0300

    Added middleware sets default response content type

---
+++ middleware/content_type_default/content_type_default.go
@@ -0,0 +1,68 @@
+package content_type_default
+
+//import "gopkg.in/webnice/debug.v1"
+//import "gopkg.in/webnice/log.v2"
+import (
+	"io"
+	"net/http"
+
+	"gopkg.in/webnice/web.v1/header"
+	"gopkg.in/webnice/web.v1/status"
+)
+
+// Interface is an interface of package
+type Interface interface {
+	// DefaultContentType Middleware set default content-type header
+	DefaultContentType(hndl http.Handler) http.Handler
+}
+
+type impl struct {
+	Default string
+}
+
+type rewr struct {
+	http.ResponseWriter
+	isHeaderWritten bool
+	Writer          io.Writer
+	Default         string
+}
+
+func New(def string) Interface {
+	var dct = &impl{
+		Default: def,
+	}
+	return dct
+}
+
+// DefaultContentType Middleware set default content-type header
+func (dct *impl) DefaultContentType(hndl http.Handler) http.Handler {
+	var fn = func(wr http.ResponseWriter, rq *http.Request) {
+		var rwr = &rewr{
+			ResponseWriter: wr,
+			Writer:         wr,
+			Default:        dct.Default,
+		}
+		hndl.ServeHTTP(rwr, rq)
+	}
+	return http.HandlerFunc(fn)
+}
+
+// WriteHeader Write header code
+func (rwr *rewr) WriteHeader(code int) {
+	if rwr.isHeaderWritten {
+		return
+	}
+	if rwr.Header().Get(header.ContentType) == "" {
+		rwr.Header().Add(header.ContentType, rwr.Default)
+	}
+	rwr.isHeaderWritten = true
+}
+
+// Write Implementation of an interface io.Writer
+func (rwr *rewr) Write(p []byte) (n int, err error) {
+	if !rwr.isHeaderWritten {
+		rwr.WriteHeader(status.Ok)
+	}
+	n, err = rwr.Writer.Write(p)
+	return
+}
---
+++ middleware/content_type_default/content_type_default_test.go
@@ -0,0 +1,91 @@
+package content_type_default
+
+//import "gopkg.in/webnice/debug.v1"
+//import "gopkg.in/webnice/log.v2"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"gopkg.in/webnice/web.v1/header"
+	"gopkg.in/webnice/web.v1/mime"
+	"gopkg.in/webnice/web.v1/route"
+	"gopkg.in/webnice/web.v1/status"
+)
+
+func testContentTypeDefaultHandlerFunc1(wr http.ResponseWriter, rq *http.Request) {
+	wr.WriteHeader(status.Ok)
+	_, _ = fmt.Fprint(wr, string(status.Bytes(status.Ok)))
+	wr.WriteHeader(status.Forbidden)
+}
+
+func testContentTypeDefaultHandlerFunc2(wr http.ResponseWriter, rq *http.Request) {
+	_, _ = fmt.Fprint(wr, string("ok"))
+}
+
+func testContentTypeDefault(t *testing.T, rou route.Interface, hf http.HandlerFunc) (contentType string, err error) {
+	var srv *httptest.Server
+	var rsp *http.Response
+	var buf []byte
+
+	srv = httptest.NewServer(hf)
+	if rou != nil {
+		srv.Config.Handler = rou
+	}
+	defer srv.Close()
+
+	rsp, err = http.Get(srv.URL)
+	if err != nil {
+		err = fmt.Errorf("response HandlerFunc error: %s", err)
+		return
+	}
+	defer rsp.Body.Close()
+
+	if buf, err = ioutil.ReadAll(rsp.Body); err != nil {
+		err = fmt.Errorf("read response error: %s", err)
+		return
+	}
+	if rsp.StatusCode != 200 {
+		err = fmt.Errorf("response staus code: %d, text: %s", rsp.StatusCode, string(buf))
+		return
+	}
+	contentType = rsp.Header.Get(header.ContentType)
+
+	return
+}
+
+func TestDefaultContentType(t *testing.T) {
+	var rou route.Interface
+	var err error
+	var ctd Interface
+	var ctv string
+
+	rou = route.New()
+	ctd = New(mime.TextRfc822Headers)
+	rou.Use(ctd.DefaultContentType)
+	rou.Get("/", testContentTypeDefaultHandlerFunc1)
+	ctv, err = testContentTypeDefault(t, rou, testContentTypeDefaultHandlerFunc1)
+	if err != nil || ctv != mime.TextRfc822Headers {
+		t.Errorf("error: %v", err)
+	}
+
+	rou = route.New()
+	ctd = New(mime.ApplicationMsgpack)
+	rou.Use(ctd.DefaultContentType)
+	rou.Get("/", testContentTypeDefaultHandlerFunc1)
+	ctv, err = testContentTypeDefault(t, rou, testContentTypeDefaultHandlerFunc1)
+	if err != nil || ctv != mime.ApplicationMsgpack {
+		t.Errorf("error: %v", err)
+	}
+
+	rou = route.New()
+	ctd = New(mime.ImageXICON)
+	rou.Use(ctd.DefaultContentType)
+	rou.Get("/", testContentTypeDefaultHandlerFunc2)
+	ctv, err = testContentTypeDefault(t, rou, testContentTypeDefaultHandlerFunc2)
+	if err != nil || ctv != mime.ImageXICON {
+		t.Errorf("error: %v", err)
+	}
+}
