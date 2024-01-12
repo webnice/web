b38a79aa37803d6d83dcb317f60c49586b498b7c
Alex Geer <monoflash@gmail.com>
2024-01-12 17:44:17 +0300

    Обновлена функция разбора адреса.

--- utilities.go
+++ utilities.go
@@ -13,15 +13,22 @@ func parseAddress(addr string) (ret *Configuration, err error) {
 		n  int
 	)
 
-	ret = new(Configuration)
-	if sp = strings.Split(addr, bColon); len(sp) <= 1 {
-		ret.Host = sp[0]
+	addr = strings.TrimSpace(addr)
+	ret, sp = new(Configuration), make([]string, 2)
+	if sp[0], sp[1], err = net.SplitHostPort(addr); err != nil {
+		ret.Host, err = addr, nil
 		return
 	}
 	if n, err = net.LookupPort(netTcp, strings.Join(sp[1:], bColon)); err != nil {
 		return
 	}
 	ret.Host, ret.Port = sp[0], uint16(n)
+	switch sp, err = net.LookupHost(sp[0]); err {
+	case nil:
+		ret.Host = sp[0]
+	default:
+		err = nil
+	}
 
 	return
 }
